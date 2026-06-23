3faaccb94197255abf54a955719e54d4e16e8f33
abulkhair_baigel <abulkhairbaigel@gmail.com>
2026-06-23 20:20:54 +0500

    Перед изменением

--- settings.py
+++ settings.py
@@ -27,8 +27,8 @@ class Settings:
 
     def initialize_dynamic_settings(self):
         """Инициализирует настройки, изменяющиеся в ходе игры."""
-        self.ship_speed = 1.75
-        self.bullet_speed = 2.75
+        self.ship_speed = 1.5
+        self.bullet_speed = 2.5
         self.alien_speed = 1.0
 
         # fleet_direction = 1 обозначает движение вправо; а и -1 - влево.
